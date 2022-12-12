c9f423ee9464e44ef0f704f0b009e21917bb4f76
stormytuna <stormytuna@outlook.com>
2022-12-12 15:20:52 +0000

    Add 400 error handling for GET /api/reviews/:review_id endpoint

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { getCategories } = require("./controllers/controllers.categories");
-const { handle404s, handle500s, handleCustomErrors } = require("./controllers/controllers.errors");
+const { handle404s, handle500s, handleCustomErrors, handlePSQLErrors } = require("./controllers/controllers.errors");
 const { getReviews, getReviewById } = require("./controllers/controllers.reviews");
 const app = express();
 
@@ -10,6 +10,7 @@ app.get("/api/reviews", getReviews);
 app.get("/api/reviews/:review_id", getReviewById);
 
 app.use(handleCustomErrors);
+app.use(handlePSQLErrors);
 app.use("/*", handle404s);
 app.use(handle500s);
 
--- controllers/controllers.errors.js
+++ controllers/controllers.errors.js
@@ -6,6 +6,14 @@ exports.handleCustomErrors = (err, req, res, next) => {
 	}
 };
 
+exports.handlePSQLErrors = (err, req, res, next) => {
+	if (err.code === "22P02") {
+		res.status(400).send({ msg: "Bad request" });
+	} else {
+		next(err);
+	}
+};
+
 exports.handle404s = (req, res, next) => {
 	res.status(404).send({ msg: "Content not found" });
 };
