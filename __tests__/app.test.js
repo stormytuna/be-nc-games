d0adc25f3faa12c5bbba275bd56f1ebf733ce244
stormytuna <stormytuna@outlook.com>
2022-12-12 12:17:38 +0000

    Add general 404 test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,7 +2,7 @@ const request = require("supertest");
 const seed = require("../db/seeds/seed");
 const data = require("../db/data/test-data");
 const app = require("../app");
-const db = require("../db");
+const db = require("../db/connection");
 
 beforeEach(() => {
 	return seed(data);
@@ -12,4 +12,10 @@ afterAll(() => {
 	if (db.end) {
 		return db.end();
 	}
+});
+
+describe("404s correctly", () => {
+	test("status:404, responds with an appropriate error message", () => {
+		return request(app).get("/api/totally-a-real-endpoint").expect(404);
+	});
 });
