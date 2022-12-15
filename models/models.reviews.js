a57398d3f3e1d596d497257c57c24171d07f1e57
stormytuna <stormytuna@outlook.com>
2022-12-15 12:35:58 +0000

    Update endpoint to fetch review_body as well

--- models/models.reviews.js
+++ models/models.reviews.js
@@ -15,7 +15,7 @@ exports.selectReviews = () => {
 
 exports.selectReviewById = (reviewId) => {
 	const query = `
-    SELECT reviews.owner, reviews.title, reviews.review_id, reviews.category, review_img_url, reviews.created_at, reviews.votes, reviews.designer, COUNT(comment_id) as comment_count
+    SELECT reviews.owner, reviews.title, reviews.review_body, reviews.review_id, reviews.category, review_img_url, reviews.created_at, reviews.votes, reviews.designer, COUNT(comment_id) as comment_count
     FROM reviews
     LEFT JOIN comments ON reviews.review_id = comments.review_id
     WHERE reviews.review_id = $1
