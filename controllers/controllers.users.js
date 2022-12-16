c7e23225938cd826f1c022670305c6c434eab0a2
stormytuna <stormytuna@outlook.com>
2022-12-16 11:02:57 +0000

    Add base GET /api/users/:username endpoint

--- controllers/controllers.users.js
+++ controllers/controllers.users.js
@@ -1,7 +1,15 @@
-const { selectUsers } = require("../models/models.users");
+const { selectUsers, selectUserByUsername } = require("../models/models.users");
 
 exports.getUsers = (req, res, next) => {
 	selectUsers().then((users) => {
 		res.status(200).send({ users });
 	});
+};
+
+exports.getUserByUsername = (req, res, next) => {
+	selectUserByUsername(req.params.username)
+		.then((user) => {
+			res.status(200).send({ user });
+		})
+		.catch(next);
 };
