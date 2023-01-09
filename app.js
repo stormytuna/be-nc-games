223b93e49d3d36a8873bba787c8a60593d33b8b9
stormytuna <stormytuna@outlook.com>
2023-01-09 10:35:47 +0000

    Add CORS package

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const { handleCustomErrors, handlePSQLErrors, handle404s, handle500s } = require("./controllers/controllers.errors");
 const apiRouter = require("./routers/api-router");
 const categoriesRouter = require("./routers/categories-router");
@@ -7,6 +8,7 @@ const reviewsRouter = require("./routers/reviews-router");
 const usersRouter = require("./routers/users-router");
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", apiRouter);
